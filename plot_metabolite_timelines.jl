f5f8da9794756e6a38961f8bfab6f1acdf3f25f6
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-14 16:25:22 -0600

    Issue #3
    Put a legend on the figure.

--- plot_metabolite_timelines.jl
+++ plot_metabolite_timelines.jl
@@ -1,3 +1,4 @@
+using Makie
 using CairoMakie
 using AlgebraOfGraphics
 
@@ -8,19 +9,17 @@ using .MetaboliteTimelines
 
 df = load_and_clean_metabolite_timelines()
 plt = plot_timeline_for_metabolite(df, "L-alanine")
-fig = Figure(; size = (1000, 1000))
-ax = Axis(
-    fig[1, 1];
-    xlabel = "Time",
-    ylabel = "Abundance",
-    title = "L-alanine",
-    titlesize = 36,
-    xlabelsize = 24,
-    ylabelsize = 24,
-    xticklabelsize = 18,
-    yticklabelsize = 18,
+fig = draw(
+    plt;
+    axis=(; title="L-alanine"),
+    legend=(;
+        position=:right,
+        orientation=:vertical,
+        title="Additive",
+        labelsize=14,
+        titlesize=16,
+    )
 )
-draw!(ax, plt)
 plt_filename = joinpath("output", "L-alanine.png")
 save(plt_filename, fig)
 println(first(df, 10))
