b3fbf42b48f9261471ecc3924eee34d000f09c25
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-14 15:52:44 -0600

    Issue #3
    Set the sizes of labels and tick labels.

--- plot_metabolite_timelines.jl
+++ plot_metabolite_timelines.jl
@@ -9,7 +9,17 @@ using .MetaboliteTimelines
 df = load_and_clean_metabolite_timelines()
 plt = plot_timeline_for_metabolite(df, "L-alanine")
 fig = Figure(; size = (1000, 1000))
-ax = Axis(fig[1, 1]; xlabel = "Time", ylabel = "Abundance", title = "L-alanine")
+ax = Axis(
+    fig[1, 1];
+    xlabel = "Time",
+    ylabel = "Abundance",
+    title = "L-alanine",
+    titlesize = 36,
+    xlabelsize = 24,
+    ylabelsize = 24,
+    xticklabelsize = 18,
+    yticklabelsize = 18,
+)
 draw!(ax, plt)
 plt_filename = joinpath("output", "L-alanine.png")
 save(plt_filename, fig)
