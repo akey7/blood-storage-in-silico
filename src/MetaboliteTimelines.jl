a14df69ab09cbce6be767345141c54c62b51fa83
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-14 15:16:55 -0600

    Issue #3
    Prepare data to be plotted.

--- src/MetaboliteTimelines.jl
+++ src/MetaboliteTimelines.jl
@@ -2,6 +2,8 @@ module MetaboliteTimelines
 
 using CSV
 using DataFrames
+using DataFramesMeta
+using Statistics
 
 export load_and_clean_metabolite_timelines
 
@@ -14,7 +16,15 @@ function load_and_clean_metabolite_timelines()
         variable_name = :Metabolite,
         value_name = :PeakAreaTop,
     )
-    return df_2
+    df_3 = @combine(
+        groupby(df_2, [:Time, :Additive, :Metabolite]),
+        :MedianPeakAreaTop = median(skipmissing(:PeakAreaTop))
+    )
+    df_4 = select(df_2, Not(:Sample))
+    df_5 = innerjoin(df_4, df_3, on = [:Time, :Additive, :Metabolite])
+    df_6 = transform(df_5, [:PeakAreaTop, :MedianPeakAreaTop] => ByRow((x, y) -> x / y) => :Abundance)
+    df_7 = select(df_6, [:Time, :Additive, :Metabolite, :Abundance])
+    return df_7
 end
 
 end
