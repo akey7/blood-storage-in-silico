3d7047d6b0685f6d39f7b03d90b8f45f829a3cd5
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-14 15:44:01 -0600

    Issue #3
    Yay it draws a scatter plot.

--- plot_metabolite_timelines.jl
+++ plot_metabolite_timelines.jl
@@ -1,4 +1,16 @@
+using CairoMakie
+using AlgebraOfGraphics
+
+CairoMakie.activate!()
+
 include("src/MetaboliteTimelines.jl")
 using .MetaboliteTimelines
 
-println(first(load_and_clean_metabolite_timelines(), 10))
+df = load_and_clean_metabolite_timelines()
+plt = plot_timeline_for_metabolite(df, "L-alanine")
+fig = Figure(; size = (1000, 1000))
+ax = Axis(fig[1, 1]; xlabel = "Time", ylabel = "Abundance", title = "L-alanine")
+draw!(ax, plt)
+plt_filename = joinpath("output", "L-alanine.png")
+save(plt_filename, fig)
+println(first(df, 10))
--- src/MetaboliteTimelines.jl
+++ src/MetaboliteTimelines.jl
@@ -4,8 +4,10 @@ using CSV
 using DataFrames
 using DataFramesMeta
 using Statistics
+using CairoMakie
+using AlgebraOfGraphics
 
-export load_and_clean_metabolite_timelines
+export load_and_clean_metabolite_timelines, plot_timeline_for_metabolite
 
 function load_and_clean_metabolite_timelines()
     filename = joinpath("input", "Data Sheet 1.CSV")
@@ -22,9 +24,22 @@ function load_and_clean_metabolite_timelines()
     )
     df_4 = select(df_2, Not(:Sample))
     df_5 = innerjoin(df_4, df_3, on = [:Time, :Additive, :Metabolite])
-    df_6 = transform(df_5, [:PeakAreaTop, :MedianPeakAreaTop] => ByRow((x, y) -> x / y) => :Abundance)
+    df_6 = transform(
+        df_5,
+        [:PeakAreaTop, :MedianPeakAreaTop] => ByRow((x, y) -> x / y) => :Abundance,
+    )
     df_7 = select(df_6, [:Time, :Additive, :Metabolite, :Abundance])
     return df_7
+end
+
+function plot_timeline_for_metabolite(df, metabolite)
+    CairoMakie.activate!()
+    filtered_df = subset(df, :Metabolite => x -> x .== metabolite)
+    plt =
+        data(filtered_df) *
+        mapping(:Time, :Abundance, color = :Additive) *
+        visual(Scatter, markersize = 8)
+    return plt
 end
 
 end
