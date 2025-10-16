34945e68a08d6c073b7147de5f87bd243946235d
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-16 15:05:26 -0600

    Issue #14
    Making the plots!

--- src/TreatmentsAgainstControlMedians.jl
+++ src/TreatmentsAgainstControlMedians.jl
@@ -4,8 +4,11 @@ using CSV
 using DataFrames
 using DataFramesMeta
 using Statistics
+using AlgebraOfGraphics
+using CairoMakie
+using Makie
 
-export load_and_clean_2
+export load_and_clean_2, plot_loess_for_all_metabolites
 
 function load_and_clean_2()
     filename = joinpath("input", "Data Sheet 1.CSV")
@@ -32,6 +35,40 @@ function load_and_clean_2()
         [:Sample, :Time, :Additive, :Metabolite, :ControlMedianNormalizedIntensity],
     )
     return df7
+end
+
+function plot_loess_for_metabolite(everything_df, metabolite)
+    df = subset(everything_df, :Metabolite => x -> x .== metabolite)
+    time_points = unique(df.Time)
+    plt =
+        data(df) *
+        mapping(
+            :Time => "Time",
+            :ControlMedianNormalizedIntensity => "Control Median Normalized Intensity",
+            color = :Additive => "Additive",
+        ) *
+        (visual(Scatter; markersize = 10) + visual(Lines) + linear())
+    fig = draw(
+        plt;
+        figure = (; size = (750, 500)),
+        axis = (; title = metabolite, xticks = time_points),
+    )
+    return fig
+end
+
+function plot_loess_for_all_metabolites(df)
+    metabolites = unique(df.Metabolite)
+    for metabolite in metabolites
+        fig = plot_loess_for_metabolite(df, metabolite)
+        clean_metabolite = replace(metabolite, r"[^A-Za-z0-9]" => "_")
+        filename = joinpath(
+            "output",
+            "control_median_normalized_intensity_plots",
+            "$(clean_metabolite).png",
+        )
+        save(filename, fig)
+        println("Wrote $filename")
+    end
 end
 
 end
