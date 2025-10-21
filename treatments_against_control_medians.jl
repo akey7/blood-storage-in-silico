7d726b6b3cdddfdb08aeebe69e90cef57ee8e2f4
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-21 17:04:38 -0600

    Issue #24
    Enable all functionality.

--- treatments_against_control_medians.jl
+++ treatments_against_control_medians.jl
@@ -12,14 +12,14 @@ Random.seed!(123)
 
 df = load_and_clean_2()
 
-# df_filename = joinpath("output", "control_median_normalized_intensity.csv")
-# CSV.write(df_filename, df)
-# println("Wrote $df_filename")
-# plot_loess_for_all_metabolites(df)
-# results_df = find_significant_metabolites_additives(df)
-# results_filename = joinpath("output", "signficant_pairs.csv")
-# CSV.write(results_filename, results_df)
-# println("Wrote $results_filename")
+df_filename = joinpath("output", "control_median_normalized_intensity.csv")
+CSV.write(df_filename, df)
+println("Wrote $df_filename")
+plot_loess_for_all_metabolites(df)
+results_df = find_significant_metabolites_additives(df)
+results_filename = joinpath("output", "signficant_pairs.csv")
+CSV.write(results_filename, results_df)
+println("Wrote $results_filename")
 
 all_c_means_df, all_wide_timeseries_df, fuzzy_objectives_df =
     c_means_metabolite_trajectories(df, 10)
