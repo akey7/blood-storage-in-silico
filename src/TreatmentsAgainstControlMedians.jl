e5a2da00ede3423d9073a1ce5c84172944f9ca9d
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-23 10:19:41 -0600

    Issue #32
    Remove LOESS smoothing from this file.

--- src/TreatmentsAgainstControlMedians.jl
+++ src/TreatmentsAgainstControlMedians.jl
@@ -7,17 +7,11 @@ using Statistics
 using AlgebraOfGraphics
 using CairoMakie
 using Makie
-using GLM
 using CategoricalArrays
-using Base.Iterators
-using ThreadsX
-using MultipleTesting
 using Clustering
 
 export load_and_clean_2,
-    plot_loess_for_all_metabolites,
     test_mixed_models,
-    find_significant_metabolites_additives,
     c_means_metabolite_trajectories,
     plot_c_means_for_all_additives,
     plot_fuzzy_objectives_elbow,
@@ -67,58 +61,6 @@ function plot_loess_for_metabolite(everything_df, metabolite)
         axis = (; title = metabolite, xticks = time_points),
     )
     return fig
-end
-
-function plot_loess_for_all_metabolites(df)
-    metabolites = unique(df.Metabolite)
-    for metabolite in metabolites
-        fig = plot_loess_for_metabolite(df, metabolite)
-        clean_metabolite = replace(metabolite, r"[^A-Za-z0-9]" => "_")
-        filename = joinpath(
-            "output",
-            "control_median_normalized_intensity_plots",
-            "$(clean_metabolite).png",
-        )
-        save(filename, fig)
-        println("Wrote $filename")
-    end
-end
-
-function find_significant_metabolites_additives(everything_df)
-    control = "01-Ctrl AS3"
-    fdr_threshold = 0.05
-    frm = @formula(ControlMedianNormalizedIntensity ~ Time + AdditiveC)
-    metabolites = unique(everything_df.Metabolite)
-    additives = [
-        additive for
-        additive in unique(everything_df.Additive) if !contains(additive, control)
-    ]
-    df1 = deepcopy(everything_df)
-    df1.AdditiveC = categorical(df1.Additive)
-    pairs = vec(collect(product(additives, metabolites)))
-    rows = ThreadsX.map(pairs) do pair
-        additive, metabolite = pair
-        println(additive, " ", metabolite)
-        df2 = subset(
-            df1,
-            :Additive => x -> x .== additive .|| x .== control,
-            :Metabolite => x -> x .== metabolite,
-        )
-        df3 = select(df2, [:ControlMedianNormalizedIntensity, :Time, :AdditiveC])
-        model = lm(frm, df3)
-        ct = coeftable(model)
-        names = coefnames(model)
-        pvals_vec = ct.cols[4]
-        pvals = Dict(names .=> pvals_vec)
-        p_value =
-            isnan(pvals["AdditiveC: $additive"]) ? 1.0 : pvals["AdditiveC: $additive"]
-        return (additive = additive, metabolite = metabolite, p_value = p_value)
-    end
-    result_df = DataFrame(rows)
-    result_df.adj_p_value = adjust(result_df.p_value, BenjaminiHochberg())
-    result_df.significant = result_df.adj_p_value .< fdr_threshold
-    final_df = sort(result_df, :adj_p_value)
-    return final_df
 end
 
 function prepare_everything_df_for_clustering(everything_df, additive)
